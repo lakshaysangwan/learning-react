58240e56a47c0752849a9d307fc105d42e3b6093
Lakshay Sangwan <lakshay.ib.hisar@gmail.com>
2021-03-11 22:52:56 +0530

    learning react day 0

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hey, this is a react app.</h1>
+        <h6>hey fuck you</h6>
+      </div>
+      
+    );
+  }
+}
+
+export default App;
